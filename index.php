2a611ba2d6c8be88c577283f7ac0a7f3f4a84bcb
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2019-10-24 22:23:15 +0200

    Files upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Home | Z.A.R.O.</title>
+
+<body>
+  <header><img src="Logo_v2.png" alt="gegevens" style="width: 200px; height: auto"></header>
+  <div id="main">
+    <article>
+    Fijn dat u geinteresseert bent in ons rekenprogramma.
+    Om U kennis te laten maken met de gegevens hebben wij een proefversie voor U gemaakt.<BR>
+    Deze kunt u bereiken via de onderstaande knop. Voor functies kunt U inloggen met de volgende gegevens:<br><BR>
+      Voor leerling:<br>
+    Gebruikersnaam: TestLeerling<br>
+    Wachtwoord: TestLeerling <br><br>
+    Voor Docent:<br>
+    Gebruikersnaam: TestDocent<br>
+    Wachtwoord: TestDocent<br>
+    <br>
+    <form method='post' action='../Program/'>
+    <input type='submit' value='Ga naar de test omgeving'>
+    </form>
+    </article>
+    <nav><b>Welkom bij Z.A.R.O<b><BR><BR>
+    Wij zijn uw leverancier voor een prikkelarme rekenomgeving.
+    </nav>
+
+  </div>
+  <footer>
+  Bedankt voor het bezoeken van onze site. Wij hopen dat U alle info die U zocht heeft kunnen vinden.<br>
+  Mocht dit niet het geval zijn kunt u ons altijd een mail sturen, dan proberen wij uw vraag alsnog te beantwoorden.<br>
+  <img src="visite_kaartje_v2.png" alt="gegevens" style="width: auto; height: auto; max-heigt=25%">
+  </footer>
+</body>
+</html>
